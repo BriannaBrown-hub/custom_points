97b9021814dde451fa0bbe548b6acc6fa9ccfa12
Brianna Brown <briannabrown@Briannas-MacBook-Pro.local>
2023-06-13 16:45:08 -0700

    Adding frontend UI

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <p className="text-3xl text-gray-700 font-bold text-yellow-400">
+        Welcome!
+      </p>
+      <p className="text-gray-500 text-lg">React and Tailwind CSS in action</p>
+    </div>
+  );
+}
